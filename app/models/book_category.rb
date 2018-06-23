6631abcb2064cdbd9eb58434d6cb08438a322023
ms2sato <ms2.sato@gmail.com>
2018-06-23 12:22:50 +0900

    many_to_many_checkboxes

---
+++ app/models/book_category.rb
@@ -0,0 +1,4 @@
+class BookCategory < ApplicationRecord
+  belongs_to :book, dependent: :destroy
+  belongs_to :category, dependent: :destroy
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+book1 = Book.create!(title: 'This is test1')
+book2 = Book.create!(title: 'This is test2')
+book3 = Book.create!(title: 'This is test3')
+
+category1 = Category.create!(name: 'test1')
+category2 = Category.create!(name: 'test2')
+
+
+book1.category_ids = [category1.id]
+book1.save!
+
+book2.category_ids = [category1.id, category2.id]
+book2.save!
